e01981fc8decfaedbd772fb6a357c59234296356
javacasm <javacasm@gmail.com>
2020-07-16 13:21:29 +0200

    update

--- codigo/T5_camara_captura_imagen.py
+++ codigo/T5_camara_captura_imagen.py
@@ -1,5 +1,5 @@
 # Ejemplo basico de previsualizacion y captura con la camara
-# captest_basico_imagen.py
+# T5_camara_captura_imagen.py
 # Mas detalles en https://projects.raspberrypi.org/en/projects/getting-started-with-picamera
 
 from picamera import PiCamera
