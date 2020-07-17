4738655c41d6af90254a5ac14b2339eaa5e8d55e
javacasm <javacasm@gmail.com>
2020-07-17 20:00:32 +0200

    update

--- codigo/T5_camara_efectos.py
+++ codigo/T5_camara_efectos.py
@@ -2,6 +2,24 @@
 # T5_camara_efectos.py
 # Mas detalles en https://picamera.readthedocs.io/en/release-1.13/api_camera.html#picamera.PiCamera.image_effect
 
+"""
+Ejemplos
+
+/home/pi/Desktop/imageblur.jpg          /home/pi/Desktop/imagehatch.jpg
+/home/pi/Desktop/imagecartoon.jpg       /home/pi/Desktop/imagenegative.jpg
+/home/pi/Desktop/imagecolorbalance.jpg  /home/pi/Desktop/imagenone.jpg
+/home/pi/Desktop/imagecolorpoint.jpg    /home/pi/Desktop/imageoilpaint.jpg
+/home/pi/Desktop/imagecolorswap.jpg     /home/pi/Desktop/imagepastel.jpg
+/home/pi/Desktop/imagedeinterlace1.jpg  /home/pi/Desktop/imageposterise.jpg
+/home/pi/Desktop/imagedeinterlace2.jpg  /home/pi/Desktop/imagesaturation.jpg
+/home/pi/Desktop/imagedenoise.jpg       /home/pi/Desktop/imagesketch.jpg
+/home/pi/Desktop/imageemboss.jpg        /home/pi/Desktop/imagesolarize.jpg
+/home/pi/Desktop/imagefilm.jpg          /home/pi/Desktop/imagewashedout.jpg
+/home/pi/Desktop/imagegpen.jpg          /home/pi/Desktop/imagewatercolor.jpg
+
+"""
+
+
 from picamera import PiCamera
 from time import sleep
 import random
