50eb6f981975555fe23cf1ec1492e85e7954d0d0
javacasm <javacasm@gmail.com>
2020-07-18 01:36:54 +0200

    update

---
+++ codigo/Tut08.1_movimientoImagen.py
@@ -0,0 +1,82 @@
+"""
+Tutorial básico de pyGame
+08.2 - Movimiento de formas con teclas:
+
+Eventos de teclado:
+Propiedades:
+* key 
+    pygame.K_q ...
+    pygame.K_LEFT ...
+
+Docs: 
+* Eentos https://www.pygame.org/docs/ref/event.html
+* Keys https://www.pygame.org/docs/ref/key.html
+
+CC by SA @javacasm
+Junio 2020
+"""
+
+import pygame
+
+width = 640
+height = 400
+
+
+# 3 formas de definir los colores
+red = pygame.Color('Red')
+cyan = pygame.Color('cyan')
+blue = pygame.Color(0,0,255) # ¿alpha?
+green = (0,  255, 0)
+white = (255,255,255)
+black = (0,0,0)
+
+pygame.init() # Inicializa el entorno de pygame
+
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Movimiento rectángulo')
+
+miImagen = pygame.image.load('./images/python-logo.png') # cargamos la imagen
+
+# coordenadas del cuadrado
+x = 200
+y = 200
+
+# velocidad de movimiento
+vel = 10
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False 
+    
+    if event.type == pygame.KEYDOWN:
+            print('Teca pulsada: '+event.unicode)
+
+            if event.key == pygame.K_q: # salimos con la tecla q
+                running == False
+
+            if event.key == pygame.K_LEFT:
+                print('Movmiento izda')
+                x -= vel
+
+            if event.key == pygame.K_RIGHT:
+                print('Movmiento drcha')                
+                x += veñ
+
+            if event.key == pygame.K_UP:
+                print('Movmiento arriba')
+                y -= vel
+
+            if  event.key == pygame.K_DOWN:
+                print('Movmiento abajo')        
+                y += vel
+
+    screen.fill(black) # ponemos el fondo negro
+
+    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height)) # después dibujamos el rectángulo
+
+    pygame.display.flip()  # actualizamos la pantalla
+
+pygame.quit()     
