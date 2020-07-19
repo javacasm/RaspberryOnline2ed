d2906a6a1b1016f76200c0acb07fa91b2f2e1ec6
Javacasm <javacasm@gmail.com>
2020-07-19 17:14:27 +0200

    add

--- codigo/test_dht22.py
+++ codigo/test_dht22.py
@@ -14,10 +14,15 @@ import Adafruit_DHT as dht
 import time
 
 
-
 sensor = dht.DHT22 # Podría ser tambien un DH11
 pin = 4
 while True:
     humedad, temperatura = dht.read_retry(sensor, pin) # recuperamos los valores del sensor
-    print('Temp={0:0.1f} ºC,  Hum={1:0.1f} %'.format(temperatura, humedad))
-    time.sleep(5) # esperamos 5 segundos
+    if humedad != None and temperatura != None:
+        print('Temp={0:0.1f} ºC,  Hum={1:0.1f} %'.format(temperatura, humedad))
+        time.sleep(5) # esperamos 5 segundos
+    else:
+        print('Error de conexión. Verifique la conexiona al pin {}'.format(pin))
+
+
+
